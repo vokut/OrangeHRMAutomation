01af419968212c1e61fb6c5be974425ce447b773
Todor Tukov <ttukov@gmail.com>
2025-07-11 17:42:10 +0300

    added html reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['json', { outputFile: 'test-results/jsonReport.json' }],
-  ["allure-playwright"]],
+  reporter: [
+    //['json', { outputFile: 'test-results/jsonReport.json' }],
+    //["allure-playwright"],
+    ['html', { open: 'never' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
