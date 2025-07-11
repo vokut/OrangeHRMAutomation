33f962cfd416eea15fbf164594892936db2e4561
Todor Tukov <ttukov@gmail.com>
2025-07-11 17:26:00 +0300

    another update

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,10 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: './.env' });
+if (!process.env.CI) {
+  require('dotenv').config();
+}
+
 
 /**
  * See https://playwright.dev/docs/test-configuration.
